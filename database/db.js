790a900f356be80f4970ae75ad2f5eea489d19ed
Didier Alejandro Bernal <53128176+soutjkl@users.noreply.github.com>
2023-11-10 15:03:15 -0500

    Update db.js

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 // const mysql = require('mysql2')
 
 // const db = mysql.createConnection(process.env.DATABASE_URL)
-const db = new Sequelize('tdr-cotization-manager','9zxsikoc7x6svonav2m2','pscale_pw_G7qV1SA8M9ZkB2erqOuRrGD2PbW7V56tD9UN5i5DUB3',{
+const db = new Sequelize('tdr-cotization-manager','wrbutv4a125z136t9zrx','pscale_pw_G7qV1SA8M9ZkB2erqOuRrGD2PbW7V56tD9UN5i5DUB3',{
     host:'aws.connect.psdb.cloud',
     dialect:'mysql',
     port:'3306',
